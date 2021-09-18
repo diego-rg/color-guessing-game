591766f78f6f7f42254a3e1a92df1b94f5bd587d
diego-rg <76407123+diego-rg@users.noreply.github.com>
2021-09-18 21:06:29 +0200

    answer check on click

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ const colorSquare = document.querySelectorAll(".container__square--round-border"
 
 //Create the array with starting data
 const colorsData = ["rgb(139, 163, 123)",
-    "rgb(139, 163, 123)",
-    "rgb(139, 163, 123)",
-    "rgb(139, 163, 123)",
-    "rgb(139, 163, 123)",
-    "rgb(139, 163, 123)"
-    ];
+                    "rgb(139, 163, 123)",
+                    "rgb(139, 163, 123)",
+                    "rgb(139, 163, 123)",
+                    "rgb(139, 163, 123)",
+                    "rgb(139, 163, 123)"
+                 ];
 
 //Function that generates random colors in the array and applies them to the squares background
 const generateColors = () => {
@@ -26,13 +26,34 @@ const generateColors = () => {
         const rgb1 = Math.floor(Math.random() * 255);
         const rgb2 = Math.floor(Math.random() * 255);
         const rgb3 = Math.floor(Math.random() * 255);
-        const randromColor = `rgb(${rgb1}, ${rgb2}, ${rgb3}`;
+        const randromColor = `rgb(${rgb1}, ${rgb2}, ${rgb3})`;
         colorsData[i] = randromColor;
         colorSquare[i].style.backgroundColor = randromColor;
+        rgbData.innerText = colorsData[Math.floor(Math.random() * colorsData.length)];
     }
-}
+};
 
 generateColors();
+
+//Event to start a new game when clicking that button
+newGame.addEventListener("click", generateColors);
+
+//Check if the answer is correct
+const checkAnswer = () => {
+    for (let i=0; i<colorsData.length; i++) {
+        colorSquare[i].addEventListener("click", function(){
+            console.log(colorSquare[i].style.backgroundColor);
+            if (colorSquare[i].style.backgroundColor === rgbData.innerText) {
+                console.log("Yes");
+            } else {
+                console.log("No");
+            }
+        })
+    }
+};
+
+checkAnswer();
+
 
 // const generateColors = (num) => {
 //     const rgb1 = Math.floor(Math.random() * 255);
@@ -47,50 +68,6 @@ generateColors();
 // generateColors(3);
 // generateColors(4);
 // generateColors(5);
-
-
-
-// for (let i=0; i<=squareColor.length; i++) {
-//     const squareColor = document.querySelector(".container__square--round-border");
-//     const rgb1 = Math.floor(Math.random() * 255);
-//     const rgb2 = Math.floor(Math.random() * 255);
-//     const rgb3 = Math.floor(Math.random() * 255);
-//     const randromColor = `rgb(${rgb1}, ${rgb2}, ${rgb3})`;
-//     squareColor.style.backgroundColor = randromColor;
-//     return colorsData;
-// }
-
-// newGame.addEventListener("click", function() {
-//     const squareColorOne = document.querySelector("#square-one");
-//     const rgb1 = Math.floor(Math.random() * 255);
-//     const rgb2 = Math.floor(Math.random() * 255);
-//     const rgb3 = Math.floor(Math.random() * 255);
-//     const randromColorOne = `rgb(${rgb1}, ${rgb2}, ${rgb3})`;
-//     squareColorOne.style.backgroundColor = randromColorOne;
-// });
-
-// newGame.addEventListener("click", function() {
-//     const squareColorTwo = document.querySelector("#square-two");
-//     const rgb1 = Math.floor(Math.random() * (255 - 0)) + 0;
-//     const rgb2 = Math.floor(Math.random() * (255 - 0)) + 0;
-//     const rgb3 = Math.floor(Math.random() * (255 - 0)) + 0;
-//     const randromColorTwo = `rgb(${rgb1}, ${rgb2}, ${rgb3})`;
-//     squareColorTwo.style.backgroundColor = randromColorTwo;
-// });
-
-// newGame.addEventListener("click", function() {
-//     const squareColorThree = document.querySelector("#square-three");
-//     const rgb1 = Math.floor(Math.random() * (255 - 0)) + 0;
-//     const rgb2 = Math.floor(Math.random() * (255 - 0)) + 0;
-//     const rgb3 = Math.floor(Math.random() * (255 - 0)) + 0;
-//     const randromColorThree = `rgb(${rgb1}, ${rgb2}, ${rgb3})`;
-//     squareColorThree.style.backgroundColor = randromColorThree;
-// });
-
-
-
-
-
 
 
 //Start a new game
